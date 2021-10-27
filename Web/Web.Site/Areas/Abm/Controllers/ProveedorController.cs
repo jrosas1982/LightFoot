8661a53f7d8c5790befef7adf139db2f424fee39
NLuna19 <nluna190898@gmail.com>
2021-10-27 17:35:28 -0300

    vista de proveedor

---
+++ Web/Web.Site/Areas/Abm/Controllers/ProveedorController.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Core.Aplicacion.Interfaces;
+using Core.Dominio.AggregatesModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web.Site.Dtos;
+using Web.Site.Helpers;
+
+namespace Web.Site.Areas
+{
+    [Authorize]
+    [Area("abm")]
+    [Route("[area]/[controller]/[action]")]
+    public class ProveedorController : CustomController
+    {
+        
+
+        public ProveedorController()
+        {
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CrearEditarProveedor(int IdProveedor)
+        {
+            Proveedor proveedor;
+
+            ProveedorModel proveedorModel = new ProveedorModel()
+            {
+                
+            };
+            return View();
+        }
+
+        public async Task<IActionResult> Crear(Articulo articulo)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Editar(Articulo articulo)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Eliminar(Articulo articulo)
+        {
+            return Ok();
+        }
+
+    }
+
+}
+
---
+++ Web/Web.Site/Areas/Abm/Models/ProveedorModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Core.Dominio.AggregatesModel;
+using Web.Site.Dtos;
+
+namespace Web.Site.Areas
+{
+    public class ProveedorModel
+    {
+        public Proveedor Proveedor { get; set; }
+
+
+    }
+}
