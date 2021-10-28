6bb3ff5e3cca86c5848039cf85ea331a7336a76a
Maxi Schmidt <Maxischmidt2011@gmail.com>
2021-10-28 01:56:42 -0300

    sweetalert

--- Web/Web.Site/wwwroot/custom/Personalizado.js
+++ Web/Web.Site/wwwroot/custom/Personalizado.js
@@ -1,18 +1,9 @@
-﻿function MensajeError2(titulo, mensaje) {
-    fire({
+﻿function MensajeError(titulo, mensaje) {
+    swal({
         title: titulo,
         text: mensaje,
         type: "warning",
         confirmButtonText: "Ok",
         closeOnConfirm: false
-    });
-}
-
-function MensajeError(titulo, mensaje) {
-    Swal.fire({
-        icon: 'error',
-        title: titulo,
-        text: mensaje,
-        type: "warning",
     });
 }
