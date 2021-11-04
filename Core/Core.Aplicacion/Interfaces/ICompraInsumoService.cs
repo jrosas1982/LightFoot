509fa2bafe58a1d165af1c21faa7d6df1317af9d
Maxi Schmidt <Maxischmidt2011@gmail.com>
2021-11-04 01:21:29 -0300

    compraInsumoService

---
+++ Core/Core.Aplicacion/Interfaces/ICompraInsumoService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Dominio.AggregatesModel;
+
+namespace Core.Aplicacion.Interfaces
+{
+
+        public interface ICompraInsumoService
+        {
+            public Task<IEnumerable<CompraInsumo>> GetCompras();
+            public Task<IEnumerable<CompraInsumoDetalle>> GetCompraDetalles(int IdCompra);
+            public Task<CompraInsumo> BuscarPorId(int IdCompra);
+            public Task CrearCompra(CompraInsumo compra, IEnumerable<CompraInsumoDetalle> detalles);
+            public Task<bool> RecibirCompra(int idCompra, long nroRemito);
+            public Task<bool> AgregarPagoCompra(int idCompra, TipoPago tipoPago, decimal montoPagado);
+        }
+}
