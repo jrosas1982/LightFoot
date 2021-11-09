c6d2d6d655f2a3daddfd077f773ab88b6fefdff3
Maxi Schmidt <Maxischmidt2011@gmail.com>
2021-11-09 06:41:46 -0300

    migracion EsFabrica y ProveedorEntityTypeConfiguration

--- Core/Core.Infraestructura/Migrations/20211109094033_bool esFabrica para compras de articulos.Designer.cs
+++ Core/Core.Infraestructura/Migrations/20211109094033_bool esFabrica para compras de articulos.Designer.cs
@@ -4,14 +4,16 @@ using Core.Infraestructura;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Core.Infraestructura.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211109094033_bool esFabrica para compras de articulos")]
+    partial class boolesFabricaparacomprasdearticulos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Core/Core.Infraestructura/Migrations/20211109094033_bool esFabrica para compras de articulos.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Infraestructura.Migrations
+{
+    public partial class boolesFabricaparacomprasdearticulos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EsFabrica",
+                table: "Proveedores",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 9, 6, 40, 32, 457, DateTimeKind.Local).AddTicks(1719));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 9, 6, 40, 32, 457, DateTimeKind.Local).AddTicks(2044));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 9, 6, 40, 32, 457, DateTimeKind.Local).AddTicks(2056));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 9, 6, 40, 32, 457, DateTimeKind.Local).AddTicks(2060));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 9, 6, 40, 32, 457, DateTimeKind.Local).AddTicks(2064));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 9, 6, 40, 32, 457, DateTimeKind.Local).AddTicks(2068));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EsFabrica",
+                table: "Proveedores");
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 8, 3, 32, 54, 816, DateTimeKind.Local).AddTicks(4838));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 8, 3, 32, 54, 816, DateTimeKind.Local).AddTicks(5173));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 8, 3, 32, 54, 816, DateTimeKind.Local).AddTicks(5186));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 8, 3, 32, 54, 816, DateTimeKind.Local).AddTicks(5191));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 8, 3, 32, 54, 816, DateTimeKind.Local).AddTicks(5195));
+
+            migrationBuilder.UpdateData(
+                table: "EtapasOrdenProduccion",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "FechaCreacion",
+                value: new DateTime(2021, 11, 8, 3, 32, 54, 816, DateTimeKind.Local).AddTicks(5200));
+        }
+    }
+}
