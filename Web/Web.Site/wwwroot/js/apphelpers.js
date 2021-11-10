684d25418bd88c023957c6a6c43c446f2f01196f
Juan Pablo <jrosas1982@gmail.com>
2021-11-09 23:07:56 -0300

    no permite ingresar letras en campos numericos en control de stock y limita a 8 los caracteres de los inputs

---
+++ Web/Web.Site/wwwroot/js/apphelpers.js
@@ -0,0 +1,14 @@
+﻿
+function valideKey(evt) {
+
+    // code is the decimal ASCII representation of the pressed key.
+    var code = (evt.which) ? evt.which : evt.keyCode;
+
+    if (code == 8) { // backspace.
+        return true;
+    } else if (code >= 48 && code <= 57) { // is a number.
+        return true;
+    } else { // other keys.
+        return false;
+    }
+};
