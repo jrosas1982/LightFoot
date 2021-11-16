739dc3f0c161a9ac2e52ee6f5f8beb58eb203d4f
NLuna19 <nluna190898@gmail.com>
2021-11-16 00:09:18 -0300

    Vista ventas index

---
+++ Web/Web.Site/Areas/Sucursal/Controllers/VentaController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Core.Aplicacion.FIlters;
+using Core.Aplicacion.Interfaces;
+using Core.Dominio.AggregatesModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Web.Site.Dtos;
+using Web.Site.Helpers;
+using System;
+using Core.Dominio.CoreModelHelpers;
+using System.Collections.Generic;
+
+namespace Web.Site.Areas
+{
+    [Area("sucursal")]
+    [Route("[area]/[controller]/[action]")]
+    public class VentaController : CustomController
+    {
+        public VentaController()
+        {
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
