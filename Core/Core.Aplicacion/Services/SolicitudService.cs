77e8ec4f7c68cb86d5cc078ae92178354cff4123
Maxi Schmidt <Maxischmidt2011@gmail.com>
2021-11-16 22:40:23 -0300

    fix index solicitudes

--- Core/Core.Aplicacion/Services/SolicitudService.cs
+++ Core/Core.Aplicacion/Services/SolicitudService.cs
@@ -205,23 +205,18 @@ namespace Core.Aplicacion.Services
         {
             var options = new MemoryCacheEntryOptions() { SlidingExpiration = TimeSpan.FromSeconds(10) };
 
-
             var solicitudesList = _db.Solicitudes
                 .OrderByDescending(x => x.EstadoSolicitud == EstadoSolicitud.PendienteAprobacion)
                 .ThenByDescending(x => x.FechaModificacion.HasValue)
                 .ThenByDescending(x => x.FechaCreacion)
                 .IncludeOptimized(x => x.Sucursal)
                 .IncludeOptimized(x => x.Sucursal.Nombre)                
-                .FromCache(options).ToList();
-
-            var solicitudesList2 = _db.Solicitudes
                 .IncludeOptimized(x => x.SolicitudDetalles)
                 .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.Articulo))
                 .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.Articulo.ArticuloCategoria))
                 .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.Articulo.ArticuloCategoria.Descripcion))
                 .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.OrdenesProduccion.Select(o => o.Id)))
-                .FromCache(options).ToList();
-
+                .ToList();
 
             //.ToListAsync()
             //.FromCacheAsync(options);
@@ -248,23 +243,24 @@ namespace Core.Aplicacion.Services
             try
             {
                 var solicitudesList = _db.Solicitudes
-                   .AsNoTracking()
-                   .Include(x => x.Sucursal)
-                   .Include(x => x.SolicitudDetalles.OrderBy(x => x.Articulo.Nombre))
-                       .ThenInclude(x => x.Articulo)
-                            .ThenInclude(x => x.ArticuloCategoria)
-                   .Include(x => x.SolicitudDetalles)
-                       .ThenInclude(x => x.OrdenesProduccion)
                     .OrderByDescending(x => x.EstadoSolicitud == EstadoSolicitud.PendienteAprobacion)
                     .ThenByDescending(x => x.FechaModificacion.HasValue)
-                    .ThenByDescending(x => x.FechaCreacion);
+                    .ThenByDescending(x => x.FechaCreacion)
+                    .IncludeOptimized(x => x.Sucursal)
+                    .IncludeOptimized(x => x.Sucursal.Nombre)
+                    .IncludeOptimized(x => x.SolicitudDetalles)
+                    .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.Articulo))
+                    .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.Articulo.ArticuloCategoria))
+                    .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.Articulo.ArticuloCategoria.Descripcion))
+                    .IncludeOptimized(x => x.SolicitudDetalles.Select(d => d.OrdenesProduccion.Select(o => o.Id)));
 
                 var solicitudesFiltered = solicitudesList.Where(x => x.FechaCreacion > filter.FechaDesde
-                                                                  && x.FechaCreacion < filter.FechaHasta.AddDays(1));
-                if (filter.IdSucursalList.Any())
-                    solicitudesFiltered = solicitudesList.Where(x => filter.IdSucursalList.Contains(x.IdSucursal));
-                if (filter.EstadoSolicitudList.Any())
-                    solicitudesFiltered = solicitudesFiltered.Where(x => filter.EstadoSolicitudList.Contains(x.EstadoSolicitud));      
+                                                                  && x.FechaCreacion < filter.FechaHasta);
+
+                if (filter.IdSucursalList != null && filter.IdSucursalList.Any())
+                    solicitudesFiltered = solicitudesList.Where(x => filter.IdSucursalList.Any(f => f == x.IdSucursal));
+                if (filter.EstadoSolicitudList != null && filter.EstadoSolicitudList.Any())
+                    solicitudesFiltered = solicitudesFiltered.Where(x => filter.EstadoSolicitudList.Any(f => f == x.EstadoSolicitud));      
 
                 _logger.LogInformation("Se buscaron las solicitudes");
                 return await solicitudesFiltered.ToListAsync();
