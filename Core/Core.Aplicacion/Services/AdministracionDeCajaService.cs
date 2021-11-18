a9c0d6ab47c7fc6a3777802e3dd86f56256a64f7
Maxi Schmidt <Maxischmidt2011@gmail.com>
2021-11-18 09:40:08 -0300

    migracion y backend cajaSucursal

---
+++ Core/Core.Aplicacion/Services/AdministracionDeCajaService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Aplicacion.Helpers;
+using Core.Aplicacion.Interfaces;
+using Core.Dominio.AggregatesModel;
+using Core.Infraestructura;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Aplicacion.Services
+{
+    public class AdministracionDeCajaService : IAdministracionDeCajaService
+    {
+        private readonly AppDbContext _db;
+        private readonly ILogger<AdministracionDeCajaService> _logger;
+
+        public AdministracionDeCajaService(ExtendedAppDbContext extendedAppDbContext, ILogger<AdministracionDeCajaService> logger)
+        {
+            _db = extendedAppDbContext.context;
+            _logger = logger;
+        }
+
+        public async Task AgregarMovimiento(CajaSucursal cajaSucursal)
+        {
+            _db.CajaSucursales.Add(cajaSucursal);
+
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<CajaSucursal> BuscarPorId(int idCajaSucursal)
+        {
+            int idSucursal = int.Parse(_db.GetSucursalId());
+
+            var movimiento = await _db.CajaSucursales
+                .Where(x => x.IdSucursal == idSucursal)
+                .Include(x => x.Sucursal)
+                .SingleOrDefaultAsync(x => x.Id == idCajaSucursal);
+                
+            return movimiento;
+        }
+
+        public async Task<IEnumerable<CajaSucursal>> GetMovimientos()
+        {
+            int idSucursal = int.Parse(_db.GetSucursalId());
+
+            var movimientosList = await _db.CajaSucursales
+                .Where(x => x.IdSucursal == idSucursal)
+                .Include(x => x.Sucursal)
+                .OrderByDescending(x => x.FechaModificacion)
+                .ThenByDescending(x => x.FechaCreacion)                
+                .ToListAsync();
+
+            return movimientosList;
+        }
+    }
+}
