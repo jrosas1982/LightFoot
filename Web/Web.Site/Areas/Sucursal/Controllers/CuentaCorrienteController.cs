ca89bef1dad920c147c473b29629b4e5590d674d
Juan Pablo <jrosas1982@gmail.com>
2021-11-20 09:45:55 -0300

    Cuenta corriente

--- Web/Web.Site/Areas/Sucursal/Controllers/CuentaCorrienteController.cs
+++ Web/Web.Site/Areas/Sucursal/Controllers/CuentaCorrienteController.cs
@@ -15,25 +15,71 @@ namespace Web.Site.Areas
     public class CuentaCorrienteController : CustomController
     {
         public ICuentaCorrienteService _cuentaCorrienteService;
+        public IVentaService _ventaService;
         public IMapper _mapper;
-        public CuentaCorrienteController(ICuentaCorrienteService cuentaCorrienteService, IMapper mapper)
+        public CuentaCorrienteController(ICuentaCorrienteService cuentaCorrienteService, IMapper mapper, IVentaService ventaService)
         {
             _cuentaCorrienteService = cuentaCorrienteService;
             _mapper = mapper;
+            _ventaService = ventaService;
         }
         public async Task<IActionResult> IndexAsync()
         {
             var cuentaCorriente = await _cuentaCorrienteService.GetCuentaCorrientes();
+
+            var ventas = await _ventaService.GetVentas();
+
+
+            var cuentaAgrupada = cuentaCorriente.GroupBy(x => x.IdCliente).Select(b => new {
+                IdCliente = b.Key,
+                TotalRecibido = b.Sum(x => x.MontoPercibido)
+            }).ToList();
+
+            var agrupaVenta = ventas.GroupBy(x => x.IdCliente).Select(sp => new {
+                IdCliente = sp.Key,
+                MontoTotalCompras = sp.Sum(m => m.MontoTotal)
+            }).ToList();
+            
+            var CuentasModelo = cuentaAgrupada.Join(agrupaVenta, c => c.IdCliente, v => v.IdCliente, (c, v) => new CuentaCorrienteModel
+            {
+                IdCliente = v.IdCliente,
+                NombreCliente = cuentaCorriente.Where(x => x.Cliente.Id == v.IdCliente).Select(x => x.Cliente.Nombre).FirstOrDefault().ToString(),
+                TotalFacturado = v.MontoTotalCompras,              
+                TotalCobrado = c.TotalRecibido,
+                DeudaTotal = v.MontoTotalCompras - c.TotalRecibido
+            }).ToList();
+
+
             var cuentasCorrientes = new List<CuentaCorrienteModel>();
-            foreach (var item in cuentaCorriente)
+
+            foreach (var item in CuentasModelo)
             {
                 var clienteCuentasCorriente = new CuentaCorrienteModel();
-                cuentasCorrientes.Add(_mapper.Map<CuentaCorrienteModel>(item));
+                clienteCuentasCorriente.IdCliente = item.IdCliente;
+                clienteCuentasCorriente.NombreCliente = item.NombreCliente;
+                clienteCuentasCorriente.TotalPagado = item.TotalPagado;
+                clienteCuentasCorriente.TotalFacturado = item.TotalFacturado;
+                clienteCuentasCorriente.TotalCobrado = item.TotalCobrado;
+                clienteCuentasCorriente.DeudaTotal = item.DeudaTotal;
+                cuentasCorrientes.Add(clienteCuentasCorriente);
             }
 
             return View(cuentasCorrientes);
         }
 
+        public async Task<IActionResult> CargarPagosRecibidos(int IdCliente)
+        {
+            var cuentaCorrienteCliente = await _cuentaCorrienteService.GetCuentaCorrientesPorCliente(IdCliente);
+            return PartialView("_PagosClienteDetalle", cuentaCorrienteCliente);
+          
+        }
+        public async Task<IActionResult> CargarComprasCliente(int IdCliente)
+        {
+            var cuentaCorrienteCliente = await _ventaService.GetVentasPorCliente(IdCliente);
+            return PartialView("_ComprasClienteDetalle", cuentaCorrienteCliente);
 
+        }
+
+        
     }
 }
